b7566c5789dd6a92498b8f27d8d1008f3652eb44
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-06-10 11:11:49 -0300

    coloquei js

---
+++ script.js
@@ -0,0 +1,56 @@
+function choose1() {
+    let escolhido = document.querySelector("#um");
+    escolhido.classList.toggle("escolhe");
+}
+function choose2() {
+    let escolhido = document.querySelector("#dois");
+    escolhido.classList.toggle("escolhe");
+}
+function choose3() {
+    let escolhido = document.querySelector("#tres");
+    escolhido.classList.toggle("escolhe");
+}
+function choose4() {
+    let escolhido = document.querySelector("#quatro");
+    escolhido.classList.toggle("escolhe");
+}
+function choose5() {
+    let escolhido = document.querySelector("#cinco");
+    escolhido.classList.toggle("escolhe");
+}
+function choose6() {
+    let escolhido = document.querySelector("#seis");
+    escolhido.classList.toggle("escolhe");
+}
+function choose7() {
+    let escolhido = document.querySelector("#sete");
+    escolhido.classList.toggle("escolhe");
+}
+function choose8() {
+    let escolhido = document.querySelector("#oito");
+    escolhido.classList.toggle("escolhe");
+}
+function choose9() {
+    let escolhido = document.querySelector("#nove");
+    escolhido.classList.toggle("escolhe");
+}
+function choose10() {
+    let escolhido = document.querySelector("#dez");
+    escolhido.classList.toggle("escolhe");
+}
+function choose11() {
+    let escolhido = document.querySelector("#onze");
+    escolhido.classList.toggle("escolhe");
+}
+function choose12() {
+    let escolhido = document.querySelector("#doze");
+    escolhido.classList.toggle("escolhe");
+}
+function choose13() {
+    let escolhido = document.querySelector("#treze");
+    escolhido.classList.toggle("escolhe");
+}
+function choose14() {
+    let escolhido = document.querySelector("#quatorze");
+    escolhido.classList.toggle("escolhe");
+}
