3221df8e0ff813cd6ab795017bb52852ebc6e5e1
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-06-12 22:30:51 -0300

    100% Requisitos

--- script.js
+++ script.js
@@ -41,16 +41,40 @@ function fecharopedido () {
     arvore.classList.add('escondido')
     let madeira = document.querySelector('.buy')
     madeira.classList.remove('escondido')
-    mensagemzap ();
+    transformanumero ();
     }
 }
 
 let valor1;
 let valor2;
 let valor3;
+let arroz;
+let feijao;
+let macarrao;
 let resposta1;
 let resposta2;
 let resposta3;
+let valortotal;
+
+function transformanumero () {
+    arroz = document.querySelector(".pratoPrincipal .escolhe .text div:nth-child(3)");
+    arroz = arroz.innerHTML;
+    valor1 = arroz.replace("R$", "");
+    valor1 = valor1.replace(",", ".");
+    feijao = document.querySelector(".bebida .escolhe .text div:nth-child(3)");
+    feijao = feijao.innerHTML;
+    valor2 = feijao.replace("R$", "");
+    valor2 = valor2.replace(",", ".");
+    macarrao = document.querySelector(".sobremesa .escolhe .text div:nth-child(3)");
+    macarrao = macarrao.innerHTML;
+    valor3 = macarrao.replace("R$", "");
+    valor3 = valor3.replace(",", ".");
+    valor1 = Number(valor1);
+    valor2 = Number(valor2);
+    valor3 = Number(valor3);
+    valortotal = valor1 + valor2 + valor3;
+    mensagemzap ();
+}
 
 function mensagemzap () {
     if (pratodopedido && bebidadopedido && sobremesadopedido) {
@@ -60,15 +84,12 @@ function mensagemzap () {
         resposta2 = vinhocaro.innerHTML;
         let sobremesacaro = document.querySelector(".sobremesa .escolhe .text div");
         resposta3 = sobremesacaro.innerHTML;
-        valor1 = document.querySelector(".pratoPrincipal .escolhe .text div:nth-child(3)");
-        valor2 = document.querySelector(".bebida .escolhe .text div:nth-child(3)");
-        valor3 = document.querySelector(".sobremesa .escolhe .text div:nth-child(3)");
+        valortotal = valortotal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
         let uri = `Olá, gostaria de fazer o pedido:
-        - Prato: ${resposta1}
-        - Bebida: ${resposta2}
-        - Sobremesa: ${resposta3}
-        
-        Total: socorro`;
+        -Prato: ${resposta1}
+        -Bebida: ${resposta2}
+        -Sobremesa: ${resposta3}
+        Total: ${valortotal}`;
         let encoded = encodeURIComponent(uri);
         const ultimaparte = document. querySelector("#meu-link");
         ultimaparte. href = `https://wa.me/5527997429980?text= ${encoded}`;
